5325a3647c8d82b512e525f78297e3004f7bd7d2
Herbert <herbert.he0229@gmail.com>
2022-04-09 16:34:00 +0800

    test :white_check_mark: : Update test

--- __test__/transformHapinToArabic.test.ts
+++ __test__/transformHapinToArabic.test.ts
@@ -19,3 +19,23 @@ test("Test TransformToArabic", () => {
     const res = transformHapinToArabic("xa")
     expect(res).toEqual("ٴا")
 })
+
+test("Test TransformToArabic", () => {
+    const res = transformHapinToArabic("xo")
+    expect(res).toEqual("ٴو")
+})
+
+test("Test TransformToArabic", () => {
+    const res = transformHapinToArabic("xe")
+    expect(res).toEqual("ٴى")
+})
+
+test("Test TransformToArabic", () => {
+    const res = transformHapinToArabic("xu")
+    expect(res).toEqual("ٴۇ")
+})
+
+test("Test TransformToArabic", () => {
+    const res = transformHapinToArabic("yu")
+    expect(res).toEqual("ٴۇ")
+})
